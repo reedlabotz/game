a5d0ae0a9eb1d4a669fd643a93d02b3772c9aa4b
RJ Marsan <rjmarsan@gmail.com>
2013-12-19 07:53:21 -0800

    Adding bare facebook project

---
+++ mobile/android/WebviewTest/src/com/whatever/webviewtest/MainActivity.java
@@ -0,0 +1,73 @@
+package com.whatever.webviewtest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // start Facebook Login
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+          // callback when session changes state
+          @Override
+          public void call(Session session, SessionState state, Exception exception) {
+            if (session.isOpened()) {
+
+              // make request to the /me API
+              Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+                // callback after Graph API response with user object
+                @Override
+                public void onCompleted(GraphUser user, Response response) {
+                  if (user != null) {
+                    Log.d("ASDF", "SUCCESSFULLY LOGGED IN "+response+ "    "+user);
+                  }
+                }
+              });
+            }
+          }
+        });
+    }
+
+    
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	
+        WebView webview = (WebView)findViewById(R.id.main_webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.loadUrl("http://game.reedlabotz.com/");
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
