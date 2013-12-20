48a31eb61af68e78b7bc2214d11ccd3ed925dde1
RJ Marsan <rjmarsan@gmail.com>
2013-12-19 18:32:55 -0800

    Adding url and javascript interface

--- mobile/android/WebviewTest/src/com/whatever/webviewtest/MainActivity.java
+++ mobile/android/WebviewTest/src/com/whatever/webviewtest/MainActivity.java
@@ -5,9 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.TextView;
 
 import com.facebook.Request;
 import com.facebook.Response;
@@ -17,6 +17,10 @@ import com.facebook.model.GraphUser;
 
 public class MainActivity extends Activity {
 
+	private WebView webview;
+	private GraphUser userInfo;
+	private Session currentSession;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,33 +32,54 @@ public class MainActivity extends Activity {
           // callback when session changes state
           @Override
           public void call(Session session, SessionState state, Exception exception) {
-            if (session.isOpened()) {
+				if (session.isOpened()) {
+					currentSession = session;
+					// make request to the /me API
+					Request.newMeRequest(session, 
+							new Request.GraphUserCallback() {
 
-              // make request to the /me API
-              Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
-
-                // callback after Graph API response with user object
-                @Override
-                public void onCompleted(GraphUser user, Response response) {
-                  if (user != null) {
-                    Log.d("ASDF", "SUCCESSFULLY LOGGED IN "+response+ "    "+user);
-                  }
-                }
-              });
-            }
-          }
+								// callback after Graph API response with user
+								// object
+								@Override
+								public void onCompleted(GraphUser user,
+										Response response) {
+									if (user != null) {
+										Log.d("ASDF", "SUCCESSFULLY LOGGED IN " + response + "    " + user);
+										userInfo = user;
+										setupWebview();
+									}
+								}
+							}).executeAsync();
+				}
+			}
         });
     }
-
+    
+    private void setupWebview() {
+        webview = (WebView)findViewById(R.id.main_webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.addJavascriptInterface(this, "hostapp");
+        webview.loadUrl(getResources().getString(R.string.app_url));
+    }
+    
+    @JavascriptInterface
+    public String getUserId() {
+    	if (userInfo == null)
+    		return null;
+    	return userInfo.getId();
+    }
+    
+    @JavascriptInterface
+    public String getAccessToken() {
+    	if (currentSession == null)
+    		return null;
+    	return currentSession.getAccessToken();
+    }
     
     @Override
     protected void onStart() {
     	super.onStart();
-    	
-        WebView webview = (WebView)findViewById(R.id.main_webview);
-        WebSettings webSettings = webview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webview.loadUrl("http://game.reedlabotz.com/");
     }
     
     @Override
